c9392b2124c48e8f38a36ce0ae4ff91499bbebf8
amster94 <ameysarode00@gmail.com>
2021-09-27 21:21:21 +0530

    import error

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap-css-only/css/bootstrap.min.css'
+import 'mdbvue/lib/css/mdb.min.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from "axios";
+
+
+axios.defaults.baseURL = "http://35.172.192.0:4060";
+
+
 
 createApp(App).use(store).use(router).mount('#app')
